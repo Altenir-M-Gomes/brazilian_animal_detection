66b85126100ee080370bf19dddd4e2d8466c10bb
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2025-08-18 06:30:45 -0300

    feat: create core itens for api

---
+++ backend/core/configs.py
@@ -0,0 +1,17 @@
+from typing import ClassVar
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+
+class Settings(BaseSettings):
+    API_V1_STR: str  = '/api/v1'
+    DB_URL: str = "postgresql+asyncpg://meu_usuario:senha123@localhost:5432/deteccao"
+    DBBaseModel: ClassVar = declarative_base() 
+
+    JWT_SECRET: str = 'segredo-trocar'
+    ALGORITHM: str = 'HS256'
+    ACESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
+    class Config:
+        case_sensitive = True
+
+settings = Settings()
