bb06cd49e03d79a2ac71c210640a5ac370b85b2c
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2025-09-21 22:23:56 -0300

    feat: update seed

--- backend/ultils/seed.py
+++ backend/ultils/seed.py
@@ -3,33 +3,29 @@ from sqlalchemy.exc import IntegrityError
 from passlib.hash import bcrypt
 from faker import Faker
 from models.usuario import UsuarioModel
-from services.usersServices import getSession  
+from configs.database import engine
 
 fake = Faker()
 
-async def seed_usuarios():
-    async with getSession() as db:
-        async with db.begin():
-            for _ in range(10):  # cria 10 usuários fake
-                nome = "Altenir"
-                sobrenome = "Modesto Gomes"
-                email = fake.unique.email()
-                senha = bcrypt.hash("senha123")  # senha padrão para todos
-                
-                usuario = UsuarioModel(
-                    nome=nome,
-                    sobrenome=sobrenome,
-                    email=email,
-                    senha=senha,
-                    ativo=True
-                )
-                db.add(usuario)
-            try:
-                await db.commit()
-                print("Seed finalizado com sucesso!")
-            except IntegrityError:
-                await db.rollback()
-                print("Algum usuário já existia, rollback realizado.")
+async def seedUsuario() -> None:
+    print("Inserindo usuário seed no banco de dados...")
+
+    async with engine.begin() as conn:
+
+        try:
+            usuario = UsuarioModel(
+                nome="Altenir",
+                sobrenome="Modesto Gomes",
+                email="altenirgomes@gmail.com",
+                senha=bcrypt.hash("senha123"),
+                ativo=True
+            )
+            conn.add(usuario)  
+            await conn.commit()  
+            print("Usuário seed criado com sucesso!")
+        except IntegrityError:
+            await conn.rollback()
+            print("Usuário já existe, rollback realizado.")
 
 if __name__ == "__main__":
-    asyncio.run(seed_usuarios())
+    asyncio.run(seedUsuario())
